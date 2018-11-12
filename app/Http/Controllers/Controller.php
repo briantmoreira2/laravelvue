5dd6ef552331ca8a38b5ab8ff38aeefa4e34350f
Briant <briant@circulo.digital>
2018-11-12 17:27:46 -0300

    feature1

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,5 +9,6 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class Controller extends BaseController
 {
+    //probando gitfow feature1
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
